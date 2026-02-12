72d9aa104b834ffe6ade9e7f8095becf3e2e686f
nrbdi <nurbaulydinara@gmail.com>
2026-02-12 22:47:05 +0500

    frontend updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,7 @@ export default defineConfig({
     server: {
         port: 3000,
         open: true,
-        proxy: {
-            '/api': {
+        proxy: {'/api': {
                 target: 'http://localhost:8080',
                 changeOrigin: true,
             },
